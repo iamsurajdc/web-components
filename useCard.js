96aab5cda3434a4e3139da0f17dd1e762ef12ad3
Suraj Chandgude <surajchandgude0304@gmail.com>
2020-10-24 17:19:34 +0530

    first commit
    Signed-off-by: Suraj Chandgude <surajchandgude0304@gmail.com>

---
+++ useCard.js
@@ -0,0 +1,24 @@
+const template = document.createElement("template");
+template.innerHTML = `
+  <style>
+    h3 {
+      color: coral
+    }
+  </style>
+  <div class="user-card">
+    <h3></h3>
+  </div>
+`;
+
+class UserCard extends HTMLElement {
+  constructor() {
+    super();
+
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector("h3").innerText = 
+    this.getAttribute("name");
+  }
+}
+
+window.customElements.define("user-card", UserCard);
